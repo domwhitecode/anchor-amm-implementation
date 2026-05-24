0aa8993db226d10a7d4f530a8b1aa2a1647ace9c
Dom <domwhitemedia@gmail.com>
2026-05-24 12:20:29 -0400

    withdraw test ix

--- programs/amm-program/tests/ix_handlers/mod.rs
+++ programs/amm-program/tests/ix_handlers/mod.rs
@@ -1,5 +1,7 @@
 mod init;
 mod deposit;
+mod withdraw;
 
 pub use init::*;
 pub use deposit::*;
+pub use withdraw::*;
---
+++ programs/amm-program/tests/ix_handlers/withdraw.rs
@@ -0,0 +1,54 @@
+use anchor_lang::solana_program::system_program::ID as SYSTEM_PROGRAM_ID;
+use anchor_lang::{InstructionData, ToAccountMetas};
+use anchor_spl::associated_token::{self, ID as ASSOCIATED_TOKEN_PROGRAM_ID, get_associated_token_address};
+use anchor_spl::token::ID as TOKEN_PROGRAM_ID;
+use litesvm::LiteSVM;
+use litesvm_token::{CreateAssociatedTokenAccount, MintTo};
+use solana_keypair::Keypair;
+use solana_message::Instruction;
+use solana_pubkey::Pubkey;
+use solana_signer::Signer;
+
+pub fn create_withdraw_ix(
+    svm: &mut LiteSVM,
+    payer: &Keypair,
+    mint_x: Pubkey,
+    mint_y: Pubkey,
+    config: Pubkey,
+    mint_lp: Pubkey,
+    vault_x: Pubkey,
+    vault_y: Pubkey,
+) -> Instruction {
+    let user = payer.pubkey();
+
+    // assuming the deposit already happened
+    let user_x = associated_token::get_associated_token_address(&user, &mint_x);
+    let user_y = associated_token::get_associated_token_address(&user, &mint_y);
+    let user_lp = associated_token::get_associated_token_address(&user, &mint_lp);
+
+    Instruction::new_with_bytes(
+        amm_program::id(),
+        &amm_program::instruction::Withdraw {
+            amount: 10_000_000,
+            min_x: 20_000_000,
+            min_y: 20_000_000,
+        }
+        .data(),
+        amm_program::accounts::Withdraw {
+            user,
+            mint_x,
+            mint_y,
+            config,
+            mint_lp,
+            vault_x,
+            vault_y,
+            user_x,
+            user_y,
+            user_lp,
+            token_program: TOKEN_PROGRAM_ID,
+            associated_token_program: ASSOCIATED_TOKEN_PROGRAM_ID,
+            system_program: SYSTEM_PROGRAM_ID,
+        }
+        .to_account_metas(None),
+    )
+}
